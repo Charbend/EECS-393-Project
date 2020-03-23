ebbaf12d615447c2f55932f1782df30b0c3448d7
wildmanlennox <47006620+wildmanlennox@users.noreply.github.com>
2020-03-23 16:45:29 -0400

    added implementation to multiple class methods

--- classes/User.java
+++ classes/User.java
@@ -10,54 +10,59 @@ public class User {
 	
 	//constructor
 	public User (String name, Type type, String pwd, String id) {
-
+		username = name;
+		usertype = type;
+		password = pwd;
+		userID = id;
 	}
 	
 	
 	//methods
-	public User getUser() {
-
-	}
 
 	public boolean confirmPassword(String pwd) {
-
+		return password.equals(pwd);
 	}
 
 	public void editUser(User user, String name, Type type) {
-
+		user.setUserName(name);
+		user.setUserType(type);
 	}
 
 	public void setUserName(String name) {
-
+		username = name;
+	}
+	
+	public String getUserName() {
+		return username;
 	}
 
 	public void setUserType(Type type) {
-
+		userType = type;
 	}
 
 	public void setUserPassword(String pwd) {
-
+		password = pwd;
 	}
 
-	Public String getUserPassword() {
-
+	public String getUserPassword() {
+		return pwd;
 	}
 
 	public Type getUserType() {
-
+		return userType;
 	}
 
 
 	public void setUserID(String id) {
-
+		userID = id;
 	}
 
 	public String getUserID() {
-
+		return userID;
 	}
 
 	public boolean makeList(String name) {
-
+		
 	}
 
 	public boolean shareList(String name, User user) {
@@ -71,12 +76,6 @@ public class User {
 	public boolean leaveList(String Name) {
 
 	}
-
-	public String getUserName() {
-		
-	}
-
-
 
 
 }
