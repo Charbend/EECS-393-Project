ad91525c0d445384787a582daed3c611d9404a5e
wildmanlennox <47006620+wildmanlennox@users.noreply.github.com>
2020-03-23 16:47:46 -0400

    added implementation for budget class methods

--- classes/Budget.java
+++ classes/Budget.java
@@ -10,28 +10,26 @@ public class Budget {
 	
 	//constructor
 	public Budget(String category) {
-
+		this.category = category;
 	}
 
 	
 	//methods
 	public void setAmount(Double value) {
-
+		amount = value;
 	}
 
 	public void increaseSpent(Double value) {
-
+		spent += value;
 	}
 
 	public double getSpent() {
-
+		return spent;
 	}
 
 	public double getAmount(Item item) {
-
+		return amount;
 	}
-
-
 
 
 
