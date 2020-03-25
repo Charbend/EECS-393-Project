74dee496fe42399ee0c58688d48d264cc1b246cf
wildmanlennox <47006620+wildmanlennox@users.noreply.github.com>
2020-03-25 14:47:22 -0400

    added implementation for multiple list class methods

--- classes/List.java
+++ classes/List.java
@@ -3,22 +3,27 @@ package classes;
 public class List {
 
 	//instance data
-	List<Item> inventory;
+	private List<Item> inventory;
+	private String title;
+	private List<User> users;
 	
 	
 	//constructors
-	public List(String name) {
-
+	public List(String name, User creator) {
+		title = name;
+		inventory = new ArrayList<Item>();
+		users = new ArrayList<User>();
+		users.add(creator)
 	}
 	
 	
 	//methods
 	public boolean addItem(Item item) {
-
+		return inventory.add(item);
 	}
 
 	public boolean removeItem(Item item) {
-
+		return inventory.remove(item);
 	}
 
 	public boolean addQuantity(int qty) {
@@ -30,13 +35,16 @@ public class List {
 	}
 
 	public boolean share(User user) {
-
+		return users.add(user);
 	}
 
 	public boolean delete() {
 		
 	}
-
+	
+	public User getUser() {
+		return users.get(0);
+	}
 
 
 }
