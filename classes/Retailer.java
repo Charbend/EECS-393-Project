184919d58821d4665f7a50a96153a060a207a75b
Sharan Mehta <sharanmehta@hotmail.com>
2020-03-26 15:25:33 -0400

    Update Retailer.java

--- classes/Retailer.java
+++ classes/Retailer.java
@@ -6,43 +6,43 @@ public class Retailer {
 	private String storeName;
 	private String storeURL;
 	
-	//constructor
+	//constructors
 	public Retailer(String name, String url) {
-
+		this.storeName = name;
+		this.storeURL = url;
+	}
+	
+	public Retailer(String name) {
+		this.storeName = name;
+		this.storeURL = null;
 	}
 	
 	
 	//methods
 	public String getRetailerName() {
-
+		return this.storeName;
 	}
 
 	public String getImage() {
-
 	}
 
 	public double getPrice() {
-
 	}
 
 	public void openRetailer(String name) {
-
 	}
 
 
 	public String getRetailerURL() {
-
+		return this.storeURL;
 	}
 
 	public void setRetailerName(String name) {
-
+		this.storeName = name;
 	}
 
 	public void setRetailerURL(String url) {
-		
+		this.storeURL = url;
 	}
-
-
-
 
 }
