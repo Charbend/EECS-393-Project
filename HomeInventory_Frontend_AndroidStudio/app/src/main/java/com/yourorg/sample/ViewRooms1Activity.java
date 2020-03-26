b4565ed87dc503ef2c7e903d673738e3102422d6
mjh244 <54736526+mjh244@users.noreply.github.com>
2020-03-26 03:56:03 -0400

    Adding Room Ability
    Users can now add rooms, but not items to those rooms.

--- HomeInventory_Frontend_AndroidStudio/app/src/main/java/com/yourorg/sample/ViewRooms1Activity.java
+++ HomeInventory_Frontend_AndroidStudio/app/src/main/java/com/yourorg/sample/ViewRooms1Activity.java
@@ -3,12 +3,49 @@ package com.yourorg.sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 
 public class ViewRooms1Activity extends AppCompatActivity {
+
+    EditText et;
+    Button bt;
+    ListView lv;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_rooms1);
+
+        et = (EditText) findViewById(R.id.editText2) ;
+        bt = (Button) findViewById(R.id.button8);
+        lv = (ListView) findViewById(R.id.listView_lv);
+
+        arrayList = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(ViewRooms1Activity.this, android.R.layout.simple_list_item_1,
+                arrayList);
+        lv.setAdapter(adapter);
+        onBtnClick();
     }
+
+    public void onBtnClick() {
+        bt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String result = et.getText().toString();
+                arrayList.add(result);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
 }
