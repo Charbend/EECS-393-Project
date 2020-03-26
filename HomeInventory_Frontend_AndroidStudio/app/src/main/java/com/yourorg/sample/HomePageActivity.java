4b9374070cfaa04cc37c5e9f23659b3beef0fa5a
mjh244 <54736526+mjh244@users.noreply.github.com>
2020-03-25 23:22:16 -0400

    View Rooms Screen
    Added View Rooms screen and signing up now leads to home page.

--- HomeInventory_Frontend_AndroidStudio/app/src/main/java/com/yourorg/sample/HomePageActivity.java
+++ HomeInventory_Frontend_AndroidStudio/app/src/main/java/com/yourorg/sample/HomePageActivity.java
@@ -1,7 +1,10 @@
 package com.yourorg.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    /** Called when the user taps the View Rooms button */
+    public void ViewRooms1(View view) {
+        Intent intent = new Intent(this, ViewRooms1Activity.class);
+        startActivity(intent);
     }
 }
--- HomeInventory_Frontend_AndroidStudio/app/src/main/java/com/yourorg/sample/SignUpActivity.java
+++ HomeInventory_Frontend_AndroidStudio/app/src/main/java/com/yourorg/sample/SignUpActivity.java
@@ -1,7 +1,10 @@
 package com.yourorg.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    /** Called when the user taps the LOG-IN button */
+    public void signUpSuccess(View view) {
+        Intent intent = new Intent(this, HomePageActivity.class);
+        startActivity(intent);
     }
 }
