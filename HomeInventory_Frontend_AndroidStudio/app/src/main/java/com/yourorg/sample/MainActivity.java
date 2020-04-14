7045263eea2affb6ad68fbca2c1098ceaadac4fb
mjh244 <54736526+mjh244@users.noreply.github.com>
2020-04-14 03:31:14 -0400

    Added my attempt to start node and URL

--- HomeInventory_Frontend_AndroidStudio/app/src/main/java/com/yourorg/sample/MainActivity.java
+++ HomeInventory_Frontend_AndroidStudio/app/src/main/java/com/yourorg/sample/MainActivity.java
@@ -34,13 +34,30 @@ public class MainActivity extends AppCompatActivity {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    startNodeWithArguments(new String[]{"node", "-e",
-                            "var http = require('http'); " +
-                                    "var versions_server = http.createServer( (request, response) => { " +
+                    // Part of my attempt to start node
+                    /*try {
+                        ProcessBuilder processBuilder = new ProcessBuilder("node server.js");
+                        processBuilder = processBuilder.directory(new File("PATH TO server.js"));
+                        Process process = processBuilder.start();
+                        BufferedReader reader = new BufferedReader(
+                                new InputStreamReader(process.getInputStream()));
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            System.out.println(line);
+                        }
+
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }*/
+
+                    /*startNodeWithArguments(new String[]{"node", "-e",
+                            "var http = require('https'); " +
+                                    "var versions_server = https.createServer( (request, response) => { " +
                                     "  response.end('Versions: ' + JSON.stringify(process.versions)); " +
                                     "}); " +
-                                    "versions_server.listen(3000);"
-                    });
+                                    "versions_server.listen(4000);"
+                    });*/
                 }
             }).start();
         }
@@ -57,7 +74,10 @@ public class MainActivity extends AppCompatActivity {
                     protected String doInBackground(Void... params) {
                         String nodeResponse="";
                         try {
-                            URL localNodeServer = new URL("localhost:3000/");
+                            // Original URL
+                            URL localNodeServer = new URL("localhost:4000/");
+                            // Address for router and emualtor?
+                            //URL localNodeServer = new URL("https://10.0.2.2:4000/");
                             BufferedReader in = new BufferedReader(
                                     new InputStreamReader(localNodeServer.openStream()));
                             String inputLine;
