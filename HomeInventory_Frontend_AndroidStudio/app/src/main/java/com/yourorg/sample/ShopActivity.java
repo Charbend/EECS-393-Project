d8525d9ad734d56f2a643924472e6c595ea66254
mjh244 <54736526+mjh244@users.noreply.github.com>
2020-04-17 08:54:19 -0400

    Added spinner and text view

--- HomeInventory_Frontend_AndroidStudio/app/src/main/java/com/yourorg/sample/ShopActivity.java
+++ HomeInventory_Frontend_AndroidStudio/app/src/main/java/com/yourorg/sample/ShopActivity.java
@@ -3,6 +3,8 @@ package com.yourorg.sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ShopActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class ShopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        // Below is to add drop down menu
+        Spinner dropdown = findViewById(R.id.spinner2);
+        // Creates a list of items for the dropdown menu.
+        String[] items = new String[]{"Amazon", "Walmart"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        // Sets dropdown menu values
+        dropdown.setAdapter(adapter);
     }
 }
