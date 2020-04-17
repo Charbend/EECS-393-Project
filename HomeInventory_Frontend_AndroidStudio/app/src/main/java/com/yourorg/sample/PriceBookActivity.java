6bd8c2396dd2511c079e16169851187f72ca5c01
mjh244 <54736526+mjh244@users.noreply.github.com>
2020-04-17 06:38:53 -0400

    Fixed Asynch errors

--- HomeInventory_Frontend_AndroidStudio/app/src/main/java/com/yourorg/sample/PriceBookActivity.java
+++ HomeInventory_Frontend_AndroidStudio/app/src/main/java/com/yourorg/sample/PriceBookActivity.java
@@ -47,62 +47,107 @@ public class PriceBookActivity extends AppCompatActivity {
         et5 = findViewById(R.id.editText12);
         bt = findViewById(R.id.button11);
         lv1 = findViewById(R.id.listView_lv5);
-        itemEntered = et1;
-        qtyEntered = et2;
-        priceEntered = et3;
-        locationEntered = et4;
-        categoryEntered = et5;
-        boolean result = true;
+
+        final String itemEntered = et1.toString();
+        final String qtyEntered = et2.toString();
+        final String priceEntered = et3.toString();
+        final String locationEntered = et4.toString();
+        final String categoryEntered = et5.toString();
+        //final boolean result = true;
 
         // Sets the list view to add text to
         arrayList1 = new ArrayList<String>();
         adapter1 = new ArrayAdapter<String>(PriceBookActivity.this, android.R.layout.simple_list_item_1,
                 arrayList1);
         lv1.setAdapter(adapter1);
-        new Async<void,void,String>() {
+        new AsyncTask<Void, Void, String>() {
             @Override
-            protectd String doInBackground(Void... params) {
-                
+            protected String doInBackground(Void... params) {
+
                 try {
-                    
-                    //instatiate the RequestQueue.
+
+                    //Instantiate the RequestQueue.
                     RequestQueue queue = Volley.newRequestQueue(PriceBookActivity.this);
-                    string url = "http://10.0.2.2:4000/pricebookitems";
+
+                    String url = "http://10.0.2.2:4000/pricebookitems";
                     StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                                new Response.Listener<String>() {
-                                    
-                                    @Override 
-                                    public void onResponse(String response) {
-                                        if (response.contains(itemEntered) && response.contains(qtyEntered) && response.contains(priceEntered) && response.contains(locationEntered) && response.contains(categoryEntered)) {
-                                            result = false;
-                                            Toast.makeText(PriceBookActivity.this, "item already exists", Toast.LENGTH_LONG).show();
+                            new Response.Listener<String>() {
+
+                                @Override
+                                public void onResponse(String response) {
+                                    String userEmail = getIntent().getStringExtra("loggedInEmail");
+                                    if (response.contains(itemEntered) && response.contains(qtyEntered) && response.contains(priceEntered) && response.contains(locationEntered) && response.contains(categoryEntered)) {
+                                        //result = false;
+                                        Toast.makeText(PriceBookActivity.this, "item already exists", Toast.LENGTH_LONG).show();
+                                    }
+
+                                    // Splits response by commas
+                                    String[] resp = response.split("\\{");
+                                    ArrayList<String> resp2 = new ArrayList<>();
+                                    for (String str1 : resp) {
+                                        String[] str1arr = str1.split(",");
+                                        for (String s1 : str1arr) {
+                                            resp2.add(s1);
                                         }
                                     }
-                                }, new Response.ErrorListener() {
-                                    @Override
-                                    public void onErrorResponse(VolleyError error) {
-                                        Toast.makeText(PriceBookActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                                    ArrayList<String> correct = new ArrayList<>();
+
+
+                                    for (int i = 0; i < resp.length; i++) {
+                                        if (resp[i].contains(userEmail)) {
+
+                                            String s = resp[i].split("itemName\":")[1];
+                                            s = s.split(",")[0];
+                                            s = s.replace("\"", "");
+                                            s = s.replace("}", "");
+                                            String s2 = resp[i].split("quantity \":")[1];
+                                            s2 = s2.split(",")[0];
+                                            s2 = s2.replace("\"", "");
+                                            s2 = s2.replace("}", "");
+                                            String s3 = "Item: " + s + ", " + "Quantity: " + s2;
+
+                                            correct.add(s3);
+
+                                        }
                                     }
+                                    if (correct.size() > 0) {
+                                        for (String str : correct) {
+                                            arrayList1.add(str);
+                                            adapter1.notifyDataSetChanged();
+                                        }
+                                        //Toast.makeText(ViewListsActivity.this, correct.toString(), Toast.LENGTH_LONG).show();
+
+                                    } else {
+                                        Toast.makeText(PriceBookActivity.this, "no pricebookitems for this user", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                                }, new Response.ErrorListener()
+
+                                {
+                                    @Override
+                                    public void onErrorResponse (VolleyError error){
+                                    Toast.makeText(PriceBookActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                                }
                                 });
                     queue.add(stringRequest);
-                } catch (Exception ex) {
-                    Toast.makeText(PriceBookActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                            } catch(Exception ex){
+                        Toast.makeText(PriceBookActivity.this, ex.toString(), Toast.LENGTH_LONG).show();
+                    }
+
+                    return null;
                 }
-                
-                return null;
-            }
-            @Override
-            protected void onPostExecute(String result){
-            }
-        }.execute()
-    }
-                                            
-                                        
-                                        
+
+                @Override
+                protected void onPostExecute (String result){
+                }
+            }.execute();
+
+
 
         onBtnClick();
 
     }
+
     // Puts user entered data into list view once button is clicked
     public void onBtnClick() {
         bt.setOnClickListener(new View.OnClickListener() {
@@ -111,75 +156,77 @@ public class PriceBookActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 // Gets text from edit text fields
-                String itemEntry = et1.getText().toString();
-                String quantityEntry = et2.getText().toString();
-                String priceEntry = et3.getText().toString();
-                String locationEntry = et4.getText().toString();
-                String categoryEntry = et5.getText().toString();
+                final String itemEntry = et1.getText().toString();
+                final String quantityEntry = et2.getText().toString();
+                final String priceEntry = et3.getText().toString();
+                final String locationEntry = et4.getText().toString();
+                final String categoryEntry = et5.getText().toString();
 
                 // Checks for empty fields
                 if (noPriceBookItemError(itemEntry, quantityEntry, priceEntry, locationEntry, categoryEntry))
-                Toast.makeText(PriceBookActivity.this, "noPriceBookItemError. Please fill all the fields ", Toast.LENGTH_LONG).show();
+                    Toast.makeText(PriceBookActivity.this, "noPriceBookItemError. Please fill all the fields ", Toast.LENGTH_LONG).show();
 
-                // Checks for invalid price entry, NEED TO GET WORKING
+                    // Checks for invalid price entry, NEED TO GET WORKING
                 /*else if (Double.parseDouble(et3.getText().toString()) == 0) {
                     (et1.getClass().getSimpleName().equals(int))
                     invalidPriceBookItemError();
                 } */
-                else if (result) {
+                else {
                     // MIGHT HAVE TO CHANGE THIS FOR MONGODB
                     // Adds the text to the list view
                     arrayList1.add("Item: " + itemEntry + ", " + "Quantity: " + quantityEntry + ", " + "Price: " +
                             priceEntry + ", " + "Location Purchased: " + locationEntry + ", " + "Category: "
                             + categoryEntry);
                     adapter1.notifyDataSetChanged();
-                    new AsyncTask<void, void, String> () {
+                    new AsyncTask<Void, Void, String>() {
                         @Override
                         protected String doInBackground(Void... params) {
+
                             try {
                                 //Instantiate the RequestQueue.
                                 RequestQueue queue = Volley.newRequestQueue(PriceBookActivity.this);
-                                string url = "http://10.0.2.2:4000/pricebookitems";
-                                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                                            new Response.Listener<String>() {
-                                                
-                                                @Override
-                                                Public void onResponse(String response) { 
-                                                    
-                                                    //textView.setText("Response is: " + response);
-                                                    Toast.makeText(PriceBookActivity.this, response, Toast.LENGTH_LONG).show();
-                                                }
-                                            }, new Response.ErrorListener() {
+                                String url = "http://10.0.2.2:4000/pricebookitems";
+                                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                                        new Response.Listener<String>() {
+
+                                            @Override
+                                            public void onResponse(String response) {
+                                                String userEmail = getIntent().getStringExtra("loggedInEmail");
+                                                //textView.setText("Response is: " + response);
+                                                Toast.makeText(PriceBookActivity.this, response, Toast.LENGTH_LONG).show();
+                                            }
+                                        }, new Response.ErrorListener() {
                                     @Override
                                     public void onErrorResponse(VolleyError error) {
                                         //textView.setText("you suck at comp sci");
                                         Toast.makeText(PriceBookActivity.this, error.toString(), Toast.LENGTH_LONG).show();
-                                       }
-                                 }){
-                                       @Override
-                                       protected Map<String, String> getParams()
-                                       {
-                                           Map<String,String> params = new HashMap<String, String>();
-                                           params.put("Item", itemEntered);
-                                           params.put("Qty", qtyEntered);
-                                           params.put("Price", priceEntered);
-                                           params.put("Location", locationEntered);
-                                           params.put("Category", categoryEntered);
-                                     
-                                           return params;
-                                       }
+                                    }
+                                }) {
+                                    @Override
+                                    protected Map<String, String> getParams() {
+                                        Map<String, String> params = new HashMap<String, String>();
+                                        String userEmail = getIntent().getStringExtra("loggedInEmail");
+                                        params.put("creator", userEmail);
+                                        params.put("itemName", itemEntry);
+                                        params.put("quantity", quantityEntry);
+                                        params.put("price", priceEntry);
+                                        params.put("location", locationEntry);
+                                        params.put("category", categoryEntry);
+
+                                        return params;
+                                    }
                                 };
                                 queue.add(postRequest);
-                                
+
                             } catch (Exception ex) {
                                 //textView.setText(ex.toString());
                                 Toast.makeText(PriceBookActivity.this, ex.toString(), Toast.LENGTH_LONG).show();
                             }
-                            
+
                             //return textView.getText().toString();
                             return null;
                         }
-                        
+
                         @Override
                         protected void onPostExecute(String result) {
                             //textView.setText(result)
@@ -187,15 +234,14 @@ public class PriceBookActivity extends AppCompatActivity {
                     }.execute();
                     try {
                         TimeUnit.SECONDS.sleep(1);
-                    } catch (InterrupedException e) {
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             }
         });
     }
-                                
-                           
+
 
     // Displays message if missing price book field submission
     public boolean noPriceBookItemError(String itemName, String itemQuantity, String itemPrice, String locationPurch, String category) {
